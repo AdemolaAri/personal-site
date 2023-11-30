87e11a2d5628af79da92e26eb80b96124757b08d
Ademola A <ademola.ari@gmail.com>
2023-11-29 20:23:19 -0600

    components for different pages

---
+++ src/app/pages/blog/blog.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-blog',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './blog.component.html',
+  styleUrl: './blog.component.css'
+})
+export class BlogComponent {
+
+}
---
+++ src/app/pages/intro/intro.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-intro',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './intro.component.html',
+  styleUrl: './intro.component.css'
+})
+export class IntroComponent {
+
+}
