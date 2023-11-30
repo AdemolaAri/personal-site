e12c402f15fcbe4535f8afe7cb017c92f3571529
Ademola A <ademola.ari@gmail.com>
2023-11-29 22:51:36 -0600

    route about page to home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { IntroComponent } from './pages/intro/intro.component';
+import { AboutComponent } from './pages/about/about.component';
 
 export const routes: Routes = [
     {path: '', component: HeaderComponent, outlet: 'home'},
     {path: '', component: IntroComponent, outlet: 'intro'},
+    {path: '', component: AboutComponent, outlet: 'about-skills'},
     {path: '', component: FooterComponent, outlet: 'footer'},
 ];
