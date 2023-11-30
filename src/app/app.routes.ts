02cd189f2f2cfe9b92c017386772deb738c297f7
Ademola A <ademola.ari@gmail.com>
2023-11-29 20:11:45 -0600

    routes and global styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HeaderComponent, outlet: 'home'},
+    {path: '', component: FooterComponent, outlet: 'footer'},
+];
