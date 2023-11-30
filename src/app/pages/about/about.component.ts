641a0af643a02f2dad3dc02eb703df76bc34c5a2
Ademola A <ademola.ari@gmail.com>
2023-11-29 22:48:48 -0600

    update about component

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { SkillsComponent } from './skills/skills.component';
+ 
+const aboutMeLink ='assets/user-data/about/about-me.txt';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule, SkillsComponent],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrl: './about.component.css',
 })
 export class AboutComponent {
+  summary: string[] | undefined;
+  summaryCount: number | undefined;
 
+  ngAfterViewInit(): void {
+    this.readFile();
+  }
+
+  constructor(private http: HttpClient) {}
+
+  private readFile(): void {
+    this.http
+        .get(aboutMeLink, { responseType: 'text' })
+        .subscribe((fileContent: string) => {
+          this.summary = fileContent.split('\n').filter((paragraph) => paragraph);
+          this.summaryCount = fileContent.split(' ').length;
+        });
+  }
 }
